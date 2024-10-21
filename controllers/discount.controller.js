983460cca1fd4c8f55e29bfdad8d6a89fd0c5aff
Polo2230 <johans.polon@autonoma.edu.co>
2024-10-21 14:30:23 -0500

    corrections on bugs

--- controllers/discount.controller.js
+++ controllers/discount.controller.js
@@ -1,57 +1,57 @@
-const Discount = require("../models/discount.model");
-const Event = require("../models/event.model");
+// const Discount = require("../models/discount.model");
+// const Event = require("../models/event.model");
 
-// Crear un nuevo descuento
-exports.createDiscount = async (req, res) => {
-  try {
-    const { name, percentage, validFrom, validUntil, minCustomerAge, appliesTo } = req.body;
+// // Crear un nuevo descuento
+// exports.createDiscount = async (req, res) => {
+//   try {
+//     const { name, percentage, validFrom, validUntil, minCustomerAge, appliesTo } = req.body;
 
-    const newDiscount = new Discount({
-      name,
-      percentage,
-      validFrom,
-      validUntil,
-      minCustomerAge,
-      appliesTo
-    });
+//     const newDiscount = new Discount({
+//       name,
+//       percentage,
+//       validFrom,
+//       validUntil,
+//       minCustomerAge,
+//       appliesTo
+//     });
 
-    const savedDiscount = await newDiscount.save();
-    res.status(201).json(savedDiscount);
-  } catch (error) {
-    res.status(500).json({ message: "Error al crear descuento", error });
-  }
-};
+//     const savedDiscount = await newDiscount.save();
+//     res.status(201).json(savedDiscount);
+//   } catch (error) {
+//     res.status(500).json({ message: "Error al crear descuento", error });
+//   }
+// };
 
-// Obtener todos los descuentos
-exports.getDiscounts = async (req, res) => {
-  try {
-    const discounts = await Discount.find().populate("appliesTo");
-    res.status(200).json(discounts);
-  } catch (error) {
-    res.status(500).json({ message: "Error al obtener los descuentos", error });
-  }
-};
+// // Obtener todos los descuentos
+// exports.getDiscounts = async (req, res) => {
+//   try {
+//     const discounts = await Discount.find().populate("appliesTo");
+//     res.status(200).json(discounts);
+//   } catch (error) {
+//     res.status(500).json({ message: "Error al obtener los descuentos", error });
+//   }
+// };
 
-// Aplicar descuento a un evento
-exports.applyDiscountToEvent = async (req, res) => {
-  try {
-    const { discountId, eventId } = req.body;
+// // Aplicar descuento a un evento
+// exports.applyDiscountToEvent = async (req, res) => {
+//   try {
+//     const { discountId, eventId } = req.body;
 
-    const discount = await Discount.findById(discountId);
-    if (!discount) {
-      return res.status(404).json({ message: "Descuento no encontrado" });
-    }
+//     const discount = await Discount.findById(discountId);
+//     if (!discount) {
+//       return res.status(404).json({ message: "Descuento no encontrado" });
+//     }
 
-    const event = await Event.findById(eventId);
-    if (!event) {
-      return res.status(404).json({ message: "Evento no encontrado" });
-    }
+//     const event = await Event.findById(eventId);
+//     if (!event) {
+//       return res.status(404).json({ message: "Evento no encontrado" });
+//     }
 
-    event.discount = discount._id;
-    await event.save();
+//     event.discount = discount._id;
+//     await event.save();
 
-    res.status(200).json({ message: "Descuento aplicado al evento", event });
-  } catch (error) {
-    res.status(500).json({ message: "Error al aplicar descuento", error });
-  }
-};
+//     res.status(200).json({ message: "Descuento aplicado al evento", event });
+//   } catch (error) {
+//     res.status(500).json({ message: "Error al aplicar descuento", error });
+//   }
+// };
--- models/discount.model.js
+++ models/discount.model.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const DiscountSchema = new Schema({
-  name: String,
-  percentage: Number, // Porcentaje de descuento
-  validFrom: Date,
-  validUntil: Date,
-  minCustomerAge: { type: Number, default: 365 }, // Días desde el registro
-  appliesTo: [{ type: Schema.Types.ObjectId, ref: "Event" }], // Eventos a los que aplica
-});
+// const DiscountSchema = new Schema({
+//   name: String,
+//   percentage: Number, // Porcentaje de descuento
+//   validFrom: Date,
+//   validUntil: Date,
+//   minCustomerAge: { type: Number, default: 365 }, // Días desde el registro
+//   appliesTo: [{ type: Schema.Types.ObjectId, ref: "Event" }], // Eventos a los que aplica
+// });
 
-module.exports = mongoose.model("Discount", DiscountSchema);
+// module.exports = mongoose.model("Discount", DiscountSchema);
